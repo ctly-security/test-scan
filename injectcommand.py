8c144241334bf43253abbceb0a75b68ea80c3a05
Clement Tan <clementtly@gmail.com>
2023-12-22 16:51:45 +0800

    Update injectcommand.py

--- injectcommand.py
+++ injectcommand.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request
 app = Flask(__name__)
 
+
+
 # curl -X GET "http://localhost:5000/tainted7/touch%20HELLO"
 @app.route("/tainted7/<something>")
 def test_sources_7(something):
